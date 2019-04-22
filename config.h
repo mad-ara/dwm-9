247b5655b5785c4142ea3615652d55e4667c1bab
Alexander Courtis <alex@courtis.org>
2019-04-22 18:52:58 +1000

    personalised keybindings, colours and fonts

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+#define FONT "monospace:bold:size=11"
+static const char *fonts[]          = { FONT };
+#define COL_GRAY1 "#222222"
+#define COL_GRAY2 "#444444"
+#define COL_GRAY3 "#bbbbbb"
+#define COL_GRAY4 "#eeeeee"
+#define COL_CYAN "#005577"
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+		/*               fg         bg         border   */
+		[SchemeNorm] = { COL_GRAY3, COL_GRAY1, COL_GRAY2 },
+		[SchemeSel]  = { COL_GRAY4, COL_CYAN,  COL_CYAN  },
 };
 
 /* tagging */
@@ -28,7 +28,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -38,13 +37,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[]=",      tile },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,34 +55,51 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-b", "-fn", FONT, "-nb", COL_GRAY1, "-nf", COL_GRAY3, "-sb", COL_CYAN, "-sf", COL_GRAY4, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+/* j4-dmenu-desktop is stupid and likes its dmenu arguments all mashed together into one, with the arguments quoted/escaped */
+static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu=dmenu -i -b -fn "FONT" -nb '"COL_GRAY1"' -nf '"COL_GRAY3"' -sb '"COL_CYAN"' -sf '"COL_GRAY4"'", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
+static const char *lockcmd[] = { "xautolock", "-locknow", NULL };
+static const char *audiomutecmd[]  = { "pulseaudio-ctl", "mute", NULL };
+static const char *audiomicmutecmd[]  = { "pulseaudio-ctl", "mute-input", NULL };
+static const char *audiolowervolumecmd[]  = { "pulseaudio-ctl", "down", NULL };
+static const char *audioraisevolumecmd[]  = { "pulseaudio-ctl", "up", NULL };
+static const char *backlightdeccmd[]  = { "xbacklight", "-10", NULL };
+static const char *backlightinccmd[]  = { "xbacklight", "+10", NULL };
+static const char *twiddledisplayscmd[]  = { "xlayoutdisplay", NULL };
 
+#include <X11/XF86keysym.h>
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_Escape, view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_y,      spawn,          {.v = twiddledisplayscmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = j4dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
+	{ MODKEY,                       XK_o,      spawn,          {.v = browsercmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,6 +110,13 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0, XF86XK_AudioMute,                     spawn,          {.v = audiomutecmd } },
+	{ 0, XF86XK_AudioMicMute,                  spawn,          {.v = audiomicmutecmd } },
+	{ 0, XF86XK_AudioLowerVolume,              spawn,          {.v = audiolowervolumecmd } },
+	{ 0, XF86XK_AudioRaiseVolume,              spawn,          {.v = audioraisevolumecmd } },
+	{ 0, XF86XK_MonBrightnessDown,             spawn,          {.v = backlightdeccmd } },
+	{ 0, XF86XK_MonBrightnessUp,               spawn,          {.v = backlightinccmd } },
+	{ 0, XF86XK_Display,                       spawn,          {.v = twiddledisplayscmd } },
 };
 
 /* button definitions */
@@ -101,7 +124,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[0]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
