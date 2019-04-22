585cbcfff366cbc4f3d6549f2749630d0d01a7ff
Alexander Courtis <alex@courtis.org>
2019-04-22 21:59:12 +1000

    goodbye j4-dmenu-desktop

--- config.h
+++ config.h
@@ -9,17 +9,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-#define FONT "monospace:bold:size=11"
-static const char *fonts[]          = { FONT };
-#define COL_GRAY1 "#222222"
-#define COL_GRAY2 "#444444"
-#define COL_GRAY3 "#bbbbbb"
-#define COL_GRAY4 "#eeeeee"
-#define COL_CYAN "#005577"
+static const char *fonts[]          = { "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 		/*               fg         bg         border   */
-		[SchemeNorm] = { COL_GRAY3, COL_GRAY1, COL_GRAY2 },
-		[SchemeSel]  = { COL_GRAY4, COL_CYAN,  COL_CYAN  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -59,10 +59,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-b", "-fn", FONT, "-nb", COL_GRAY1, "-nf", COL_GRAY3, "-sb", COL_CYAN, "-sf", COL_GRAY4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-/* j4-dmenu-desktop is stupid and likes its dmenu arguments all mashed together into one, with the arguments quoted/escaped */
-static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu=dmenu -i -b -fn "FONT" -nb '"COL_GRAY1"' -nf '"COL_GRAY3"' -sb '"COL_CYAN"' -sf '"COL_GRAY4"'", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *lockcmd[] = { "xautolock", "-locknow", NULL };
 static const char *audiomutecmd[]  = { "pulseaudio-ctl", "mute", NULL };
@@ -108,7 +106,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_y,      spawn,          {.v = twiddledisplayscmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = j4dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_o,      spawn,          {.v = browsercmd } },
 	TAGKEYS(                        XK_1,                      0)
